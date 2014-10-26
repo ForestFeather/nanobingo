3ead44b97d845ce79e13a5407553503aff582394
Collin 'Ridayah' O'Connor <cdo@feather-mage.com>
2014-10-26 10:45:08 -0500

    Linking to twitters.

--- index.php
+++ index.php
@@ -17,12 +17,13 @@
   <![endif]-->
   <!--[if gt IE 8]><!-->
     <link rel="stylesheet" href="css/layouts/marketing.css">
+    <link rel="stylesheet" href="css/print.css" type="text/css" media="print" />
   <!--<![endif]-->
   <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
 </head>
 <body>
-<div class="header">
-    <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
+<div class="header no-print">
+    <div class="no-print home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
         <a class="pure-menu-heading" href="">Nano Bingo</a>
         <ul>
             <li class="pure-menu-selected"><a href="#">Home</a></li>
@@ -33,7 +34,7 @@
     </div>
 </div>
 
-<div class="splash-container">
+<div class="splash-container no-print">
     <div class="splash">
         <h1 class="splash-head">NaNoWriMo Bingo Card Generator</h1>
         <p class="splash-subhead">
@@ -104,7 +105,8 @@ echo "<tbody>";
 for( $i = 0; $i < 5; $i++ ) {
     echo "<tr>";
     for( $j = 0; $j < 5; $j++ ) {
-        echo "<td>" . $bingoItems[($i*5) + $j] . "</td>";
+        if ( isset($_GET['free']) && ($i*5 + $j) == 12 ) { echo "<td>FREE TILE</td>"; }
+        else { echo "<td>" . $bingoItems[($i*5) + $j] . "</td>"; }
         // echo "<td>SAMPLE CONTENT</td>";
     }
     echo "</tr>";
@@ -122,7 +124,7 @@ echo "</tbody>";
     </div>
 </div>
 
-<div class="ribbon l-box-lrg pure-g">
+<div class="ribbon l-box-lrg pure-g no-print">
     <!--<div class="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
         <img class="pure-img-responsive" alt="File Icons" width="300" src="img/common/file-icons.png">
     </div>-->
@@ -131,7 +133,7 @@ echo "</tbody>";
         <h2 id="Settings" class="content-head content-head-ribbon">Bingo Card Categories</h2>
 
         <p>
-            You may select your bingo card challenge groups right here.  Make it as hard (or as simple) as you want.
+            You may select your bingo card challenge groups right here.  Make it as hard (or as simple) as you want.  Clicking 'Generate' will recreate the same card seen on the page now.  Changing the seed without changing selected categories will generate a new card with the same groups as before.  If you wish to save a copy of your bingo card, just 'print' the page!  You will get your bingo card and only your bingo card.
         </p>
         <form class='pure-form' action='index.php' method="get"><fieldset>
         <div class="pure-g">
@@ -140,6 +142,9 @@ echo "</tbody>";
                 <h3 class='content-head-ribbon'>Settings</h3>
                 <label class='content-head-ribbon' for='seed'>Seed</label>
                 <input id='seed' name='seed' type='text' placeholder='Seed Value' value='<?php echo (isset($_GET['seed']) ? $_GET['seed'] : $seed); ?>'>
+                <label class='pure-checkbox content-head-ribbon' for='free'>
+                <input type='checkbox' id='free' name='free' <? echo isset($_GET['free']) ? 'checked' : '';  ?>> Free Center Tile
+                </label>
                 <div class='l-box'>
                 <h4 class='content-head-ribbon'>Include:</h4>
                 <? foreach($ListItems as $key => $value) { 
@@ -181,8 +186,8 @@ foreach($sortedListItems as $category => $items) {
         </div>
     </div>
 
-    <div class="footer l-box is-center">
-        <p>Made by Collin 'Ridayah' O'Connor with help from Alexandra 'Chomskyrabbit' McGowan. &copy; 2014.  Questions, comments?  Contact <a href='mailto:webmaster_IfItsBetweenAndIncludesUnderscoresItsGone__@feather-mage.com'>Webmaster</a></p>
+    <div class="footer l-box is-center no-print">
+        <p>Made by Collin '<a href="https://www.twitter.com/meddeveloper">Ridayah</a>' O'Connor with help from Alexandra '<a href="https://www.twitter.com/chomskyrabbit">Chomskyrabbit</a>' McGowan. &copy; 2014.  Questions, comments?  Contact <a href='mailto:webmaster_IfItsBetweenAndIncludesUnderscoresItsGone__@feather-mage.com'>Webmaster</a></p>
     </div>
 </div>
 </body>
