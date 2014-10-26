c5c71543512ffe931bf28f95ea371627f83b4f6e
Collin 'Ridayah' O'Connor <cdo@feather-mage.com>
2014-10-25 19:59:47 -0500

    Getting things aligned and sorted right.  Go figure marketing.css was the troublemaker.

--- index.php
+++ index.php
@@ -124,19 +124,21 @@ echo "</tbody>";
         <p>
             You may select your bingo card challenge groups right here.  Make it as hard (or as simple) as you want.
         </p>
-        <form class='pure-form pure-form-aligned' action='index.php' method="get"><fieldset>
+        <form class='pure-form' action='index.php' method="get"><fieldset>
         <div class="pure-g">
             <div class='pure-u-1 pure-u-md-1-3 pure-u-lg-1-5'>
                 <div class='l-box'>
                 <h3 class='content-head-ribbon'>Settings</h3>
                 <label class='content-head-ribbon' for='seed'>Seed</label>
                 <input id='seed' name='seed' type='text' placeholder='Seed Value' value='<?php echo (isset($_GET['seed']) ? $_GET['seed'] : mt_rand()); ?>'>
+                <div class='l-box'>
                 <? foreach($ListItems as $key => $value) { 
-                    echo "<div class='pure-control-group'><label class='pure-checkbox content-head-ribbon' for='".$key."'>";
+                    echo "<label class='pure-checkbox content-head-ribbon' for='".$key."'>";
                     echo "<input type='checkbox'id='".$key."' name='".$key.".'";
                     echo isset($_GET[$key]) ? 'checked' : '';
-                    echo ">".$key."</label></div>"; 
+                    echo "> ".$key."</label>"; 
                 } ?>
+                </div>
             </div>
 </div>
 <?
